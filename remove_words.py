ece0c4f542afed5be5f9777bfe2f14ebed315cbd
derek-saal <derek@saal.ai>
2018-10-14 11:31:56 +0400

    Abstracting for proper run environment

--- remove_words.py
+++ remove_words.py
@@ -5,7 +5,10 @@ from nltk.corpus import wordnet as wn
 from utils import clean_str, loadWord2Vec
 import sys
 
-# nltk.download()
+try:
+    nltk.data.find('corpora/stopwords/english')
+except LookupError:
+    nltk.download('english')
 stop_words = set(stopwords.words('english'))
 print(stop_words)
 
@@ -14,66 +17,58 @@ print(stop_words)
 # vocab, embd, word_vector_map = loadWord2Vec(word_vector_file)
 # word_embeddings_dim = len(embd[0])
 
-dataset = 'mr'
 
-doc_content_list = []
-f = open('data/corpus/' + dataset + '.txt', 'rb')
-# f = open('data/wiki_long_abstracts_en_text.txt', 'r')
-for line in f.readlines():
-    doc_content_list.append(line.strip().decode('latin1'))
-f.close()
-
-
-word_freq = {}  # to remove rare words
-
-for doc_content in doc_content_list:
-    temp = clean_str(doc_content)
-    words = temp.split()
-    for word in words:
-        if word in word_freq:
-            word_freq[word] += 1
-        else:
-            word_freq[word] = 1
-
-clean_docs = []
-for doc_content in doc_content_list:
-    temp = clean_str(doc_content)
-    words = temp.split()
-    doc_words = []
-    for word in words:
-        # and word in word_vector_map
-        if word not in stop_words and word_freq[word] >= 5: #word not in stop_words and word_freq[word] >= 5
-            doc_words.append(word)
-    doc_str = ' '.join(doc_words).strip()
-    #if doc_str == '':
-        #doc_str = temp
-    clean_docs.append(doc_str)
-
-clean_corpus_str = '\n'.join(clean_docs)
-
-f = open('data/corpus/' + dataset + '.clean.txt', 'w')
-#f = open('data/wiki_long_abstracts_en_text.clean.txt', 'w')
-f.write(clean_corpus_str)
-f.close()
-
-dataset = '20ng'
-min_len = 10000
-aver_len = 0
-max_len = 0 
-
-f = open('data/corpus/' + dataset + '.clean.txt', 'r')
-#f = open('data/wiki_long_abstracts_en_text.txt', 'r')
-lines = f.readlines()
-for line in lines:
-    line = line.strip()
-    temp = line.split()
-    aver_len = aver_len + len(temp)
-    if len(temp) < min_len:
-        min_len = len(temp)
-    if len(temp) > max_len:
-        max_len = len(temp)
-f.close()
-aver_len = 1.0 * aver_len / len(lines)
-print('min_len : ' + str(min_len))
-print('max_len : ' + str(max_len))
-print('average_len : ' + str(aver_len))
+def remove_words(dataset):
+    doc_content_list = []
+    f = open('data/corpus/' + dataset + '.txt', 'rb')
+    # f = open('data/wiki_long_abstracts_en_text.txt', 'r')
+    for line in f.readlines():
+        doc_content_list.append(line.strip().decode('latin1'))
+    f.close()
+    word_freq = {}  # to remove rare words
+    for doc_content in doc_content_list:
+        temp = clean_str(doc_content)
+        words = temp.split()
+        for word in words:
+            if word in word_freq:
+                word_freq[word] += 1
+            else:
+                word_freq[word] = 1
+    clean_docs = []
+    for doc_content in doc_content_list:
+        temp = clean_str(doc_content)
+        words = temp.split()
+        doc_words = []
+        for word in words:
+            # and word in word_vector_map
+            if word not in stop_words and word_freq[word] >= 5:  # word not in stop_words and word_freq[word] >= 5
+                doc_words.append(word)
+        doc_str = ' '.join(doc_words).strip()
+        # if doc_str == '':
+        # doc_str = temp
+        clean_docs.append(doc_str)
+    clean_corpus_str = '\n'.join(clean_docs)
+    f = open('data/corpus/' + dataset + '.clean.txt', 'w')
+    # f = open('data/wiki_long_abstracts_en_text.clean.txt', 'w')
+    f.write(clean_corpus_str)
+    f.close()
+    dataset = '20ng'
+    min_len = 10000
+    aver_len = 0
+    max_len = 0
+    f = open('data/corpus/' + dataset + '.clean.txt', 'r')
+    # f = open('data/wiki_long_abstracts_en_text.txt', 'r')
+    lines = f.readlines()
+    for line in lines:
+        line = line.strip()
+        temp = line.split()
+        aver_len = aver_len + len(temp)
+        if len(temp) < min_len:
+            min_len = len(temp)
+        if len(temp) > max_len:
+            max_len = len(temp)
+    f.close()
+    aver_len = 1.0 * aver_len / len(lines)
+    print('min_len : ' + str(min_len))
+    print('max_len : ' + str(max_len))
+    print('average_len : ' + str(aver_len))
---
+++ run.py
@@ -0,0 +1,14 @@
+from remove_words import remove_words
+from build_graph import build_graph
+from train import train
+
+dataset = 'mr'
+
+print("Running Remove Stop Words\n")
+remove_words(dataset)
+
+print("Running Build Graph\n")
+build_graph(dataset)
+
+print("Running Train\n")
+train(dataset)
